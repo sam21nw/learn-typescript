03ab6e5b995acf484cdfe2bb4b82b6ce7b41927e
Sam Jacob <sam21nw@gmail.com>
2024-09-29 07:50:51 +0200

    triangle class

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import { Triangle } from './triangle';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``;
 
-type stringOrNumber = string | number;
+type stringOrNumber = string | number | boolean;
 
 const logMsg = (msg: stringOrNumber) => { console.log(msg); };
 
-const triangle = new Triangle(23, 5);
+const triangle = new Triangle(25, 25);
 
 let area = triangle.getTriangleArea();
-logMsg(area);
-
 let hyp = triangle.getTriangleHypotenuse();
+
+logMsg(area);
 logMsg(hyp);
+logMsg(triangle instanceof Triangle);
--- src/triangle.ts
+++ src/triangle.ts
@@ -1,15 +1,19 @@
 class Triangle {
-    constructor(
-        public a: number,
-        public b: number,
-    ) {
+    constructor(private a: number, private b: number = 9,) {
+        if (!Number.isFinite(a) || a <= 0) {
+            throw new Error("Invalid");
+        }
+        if (!Number.isFinite(b) || b <= 0) {
+            throw new Error("Invalid");
+        }
+
         this.a = a;
         this.b = b;
     }
-    public getTriangleArea(): number {
+    getTriangleArea(): number {
         return this.a * this.b * .5;
     }
-    public getTriangleHypotenuse = (): number => {
+    getTriangleHypotenuse = (): number => {
         return Math.sqrt(this.a ** 2 + this.b ** 2);
     };
 }
