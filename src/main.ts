ec519ae603c262d47040a0ae6de6df00401b0279
Sam Jacob <sam21nw@gmail.com>
2024-10-01 16:42:17 +0200

    minor change in main ts file

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import Bands from './bands';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``;
 
-type stringOrNumber = string | number | boolean;
+type stringOrNumber = string | number | boolean | string[];
 
 const logMsg = (msg: stringOrNumber) => { console.log(msg); };
 
 const myBands = new Bands();
-myBands.data = ["Neil", "Led", 123];
-console.log(myBands.data);
+myBands.data = ["Neil", "Led"];
+logMsg(myBands.data);
